cd6be4ec47b5a3723f7238b0fb948f8af0ea606d
FuYen Lin <l051330@gmail.com>
2023-03-18 16:51:20 +0800

    增加區間提示，ok！

--- guess_game.py
+++ guess_game.py
@@ -4,17 +4,19 @@ x = random.randint(1, 50)
 print(x)
 
 win = False
+start, end = 1, 50
 for i in range(5):
-    y = eval(input('猜一數字(1~50:)'))
+    y = eval(input(f'猜 {start} ~ {end} 之間的數字:'))
     if x == y:
         win = True
         break
     if x > y:
-        print('猜大一點')
+        if start < y:
+            start = y
     else:
-        print('猜小一點')
-
+        if end > y:
+            end = y
 if win:
-    print(f'恭喜過關！你總共猜了{i+1}次')
+    print(f'猜中了！你猜了{i+1}次')
 else:
-    print(f'遊戲結束!答案為：{x}')
+    print(f'沒猜中，遊戲結束了! 正確答案為：{x}')
